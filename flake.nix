e9600c71da0d411953b1dbe8c81490edfd3375d4
Christopher Mühl <toki@toph.so>
2025-12-28 17:36:26 +0100

    Add nix-based devshell

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} (top: {
+      # I can't test support for other systems right now
+      systems = ["x86_64-linux"];
+
+      perSystem = {
+        lib,
+        config,
+        pkgs,
+        system,
+        ...
+      }: {
+        # Add the fenix overlay when building pkgs in flake-parts
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.fenix.overlays.default
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.openssl];
+
+          packages = with pkgs; [
+            pkg-config
+            openssl
+
+            (fenix.stable.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            rust-analyzer
+          ];
+        };
+      };
+    });
+}
